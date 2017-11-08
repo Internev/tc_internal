0d6ee906206bffd4874364bd3313447e2c097b8f
Neville Neville <nchallinger@gmail.com>
2017-11-08 17:15:23 +1100

    Update header to include menu dropdown and correct token checking.

--- client/containers/Header.js
+++ client/containers/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Dropdown } from 'semantic-ui-react'
+import { checkToken } from '../redux/creators/userCreators'
 import './Header.scss'
 
 class Header extends React.Component {
+  componentWillMount () {
+    const token = localStorage.getItem('id_token')
+    if (token) this.props.dispatch(checkToken(token))
+  }
+  componentDidUpdate () {
+    console.log('heasder props:', this.props, this.props.user.admin)
+  }
   render () {
     return (
       <header className='header'>
@@ -12,6 +21,20 @@ class Header extends React.Component {
         ? <nav className='header_nav'>
           <div className='header_nav-wide'>
             <Link to='/home' className='header_nav-wide-link'>Home</Link>
+            {
+              this.props.user.admin
+              ? <Dropdown text='Admin Tools' className='header_nav-wide-dropdown'>
+                <Dropdown.Menu>
+                  <Dropdown.Item>
+                    <Link to='/admin/assign-dogs' className='header_nav-wide-link'>Assign Dogs</Link></Dropdown.Item>
+                  <Dropdown.Item>
+                    <Link to='/admin/user-tools' className='header_nav-wide-link'>User Tools</Link></Dropdown.Item>
+                  <Dropdown.Item>
+                    <Link to='/admin/client-tools' className='header_nav-wide-link'>Client Tools</Link></Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              : null
+            }
             <Link to='/logout' className='header_nav-wide-link'>Log out</Link>
           </div>
           <div className='header_nav-narrow'>
@@ -19,6 +42,17 @@ class Header extends React.Component {
             <div className='header_nav-narrow-links'>
               <Link to='/home' className='header_nav-narrow-link'>Home</Link>
               <Link to='/logout' className='header_nav-narrow-link'>Log out</Link>
+              {
+                this.props.user.admin
+                ? <Dropdown text='Admin Tools'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item><Link to='/admin/assign-dogs'>Assign Dogs</Link></Dropdown.Item>
+                    <Dropdown.Item><Link to='/admin/user-tools'>User Tools</Link></Dropdown.Item>
+                    <Dropdown.Item><Link to='/admin/client-tools'>Client Tools</Link></Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                : null
+              }
             </div>
           </div>
         </nav>
