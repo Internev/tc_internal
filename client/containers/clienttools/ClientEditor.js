829d1999a9e02c2feb0a3ae8e14e4157222a6f3a
Neville Neville <nchallinger@gmail.com>
2017-11-17 17:26:01 +1100

    Add client search filter and basic client cards.

--- client/containers/clienttools/ClientEditor.js
+++ client/containers/clienttools/ClientEditor.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Input, Card } from 'semantic-ui-react'
 import './ClientTools.scss'
 
-const ClientEditor = ({handleClientEditor}) => (
+const ClientEditor = ({search, clients}) => (
   <div className='client_editor'>
     <h3>Edit Client Details</h3>
     <div>
-      <span>Search for a client to edit.</span>
+      <Input
+        placeholder='Search by client name or email address'
+        icon='search'
+        fluid
+        onChange={e => search(e)}
+        className='client_editor-input'
+        />
+    </div>
+    <div className='client_editor-results'>
+      <Card.Group itemsPerRow={4}>
+        {clients.map((client, i) => (
+          <Card
+            header={client.name}
+            meta={client.email}
+            description={client.dogs}
+            />
+          ))}
+      </Card.Group>
     </div>
   </div>
 )
--- client/containers/clienttools/ClientTools.js
+++ client/containers/clienttools/ClientTools.js
@@ -10,14 +10,16 @@ import './ClientTools.scss'
 class ClientTools extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {searchTerm: ''}
     this.handleClientCSVUpload = this.handleClientCSVUpload.bind(this)
+    this.handleClientEditorSearch = this.handleClientEditorSearch.bind(this)
   }
   componentDidMount () {
     console.log('Client Tools props:', this.props)
     if (this.props.clients.list.length < 1) this.props.dispatch(getClients())
   }
   componentDidUpdate () {
-    console.log('Client Tools props:', this.props)
+    console.log('Client Tools props:', this.props, this.state)
   }
   handleClientCSVUpload (e) {
     let reader = new FileReader()
@@ -26,12 +28,24 @@ class ClientTools extends React.Component {
     }
     reader.readAsText(e.target.files[0])
   }
+  handleClientEditorSearch (e) {
+    this.setState({searchTerm: e.target.value})
+  }
   render () {
     return (
       <div>
         <ClientCSVUpload handleClientCSVUpload={this.handleClientCSVUpload} />
         <hr />
-        <ClientEditor />
+        <ClientEditor
+          search={this.handleClientEditorSearch}
+          clients={this.state.searchTerm
+            ? this.props.clients.list.filter(client =>
+              this.state.searchTerm
+              ? `${client.name}${client.email}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+              : true
+            )
+            : []}
+          />
       </div>
     )
   }
