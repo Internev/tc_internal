b7be97c85a53dd452d389bc482fb54a8cea4783d
Neville Neville <nchallinger@gmail.com>
2017-12-14 17:39:04 +1100

    Add accordion for walk history compact display.

--- client/containers/history/WalkHistory.js
+++ client/containers/history/WalkHistory.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Input, Card, Icon, Modal, Header, Button, Loader } from 'semantic-ui-react'
+import { Accordion, Icon } from 'semantic-ui-react'
+import moment from 'moment'
 
-const WalkHistory = ({all}) => (
-  <div>
-    <h4>Walk History</h4>
-    {all.map((walk, i) => (
-      <div key={i}>
-        <div>
-          {walk.dogs.length} dogs walked on {walk.date}.
-        </div>
-      </div>
-    ))}
-  </div>
-)
-
+const WalkHistory = ({all, handleAccordionClick, activeIndex}) => {
+  const panels = (walks) => {
+    return walks
+    .filter(walk => walk.dogs.length)
+    .map(walk => {
+      return {
+        title: `${moment(walk.date).format('MMMM Do YYYY')}: ${walk.dogs.length} dogs walked.`,
+        content: {
+          content: (
+            <div>
+              {walk.dogs.map((dog, i) => (
+                <div key={i}><b>{dog.name}</b> (owned by {dog.client.name})</div>
+              ))}
+            </div>
+          ),
+          key: 'content-dog'
+        }
+      }
+    })
+  }
+  return (
+    <div>
+      <h4>Walk History</h4>
+      <Accordion styled defaultActiveIndex={0} panels={panels(all)} />
+    </div>
+  )
+}
 export default WalkHistory
