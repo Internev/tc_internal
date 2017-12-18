d251d3a6e41a32aaeb223e49cdecad6dc57c9bff
Neville Neville <nchallinger@gmail.com>
2017-12-18 15:43:59 +1100

    Add nested walk history display and separate current pay period.

--- client/containers/history/WalkHistory.js
+++ client/containers/history/WalkHistory.js
@@ -2,8 +2,24 @@ import React from 'react'
 import { Accordion } from 'semantic-ui-react'
 import moment from 'moment'
 
-const WalkHistory = ({all, handleAccordionClick, activeIndex}) => {
-  const panels = (walks) => {
+const WalkHistory = ({all, current, handleAccordionClick, activeIndex}) => {
+  const rootPanels = (dateBlocks) => {
+    return dateBlocks.map((dateBlock, i) => {
+      return {
+        title: `Fortnight starting ${moment(dateBlock.date).format('MMMM Do YYYY')}`,
+        content: {
+          content: (
+            <div>
+              <h4>Total {dateBlock.walks.reduce((a, w) => a += w.dogs.length, 0)} Dogs Walked</h4>
+              <Accordion.Accordion styled panels={innerPanels(dateBlock.walks)} />
+            </div>
+          )
+        },
+        key: dateBlock.date
+      }
+    })
+  }
+  const innerPanels = (walks) => {
     return walks
     .filter(walk => walk.dogs.length)
     .map(walk => {
@@ -24,9 +40,13 @@ const WalkHistory = ({all, handleAccordionClick, activeIndex}) => {
   }
   return (
     <div>
+      <h4>Current Pay Period (starts {moment(current.date).format('MMMM Do YYYY')})</h4>
+      <h4>Total {current.walks.reduce((a, w) => a += w.dogs.length, 0)} Dogs Walked</h4>
+      <div>
+        <Accordion styled panels={innerPanels(current.walks)} />
+      </div>
       <h4>Walk History</h4>
-      {// <Accordion styled defaultActiveIndex={0} panels={panels(all)} />
-      }
+      <Accordion styled panels={rootPanels(all)} />
     </div>
   )
 }
