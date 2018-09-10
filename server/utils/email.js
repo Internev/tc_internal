864ed4a53874726e2546da0d98c1dd779de5ba5c
Neville Neville <nchallinger@gmail.com>
2018-09-10 11:13:39 +1000

    Update password reset email to use ip instead of domain.

--- server/utils/email.js
+++ server/utils/email.js
@@ -8,7 +8,7 @@ const forgotPassEmail = (email, passToken) => {
   console.log('forgotPassEmail func, email:', email, 'passToken:', passToken)
   if (!email) return Promise.resolve({messageId: 0, response: 'no email address provided.'})
   let html = `<div>Hi there, someone requested a password reset on your Tom and Captain account.</div>
-  <div>Please <a href="https://staff.tomandcaptain.com/reset/${passToken}">click here</a> to reset your password. (or copy/paste this address into your browser: https://staff.tomandcaptain.com/reset/${passToken})</div>
+  <div>Please <a href="http:/45.77.234.236/reset/${passToken}">click here</a> to reset your password. (or copy/paste this address into your browser: http:/45.77.234.236/reset/${passToken})</div>
   <div>If you did not request a password reset, feel free to ignore this email, your password will remain unchanged.</div>`
 
   let mailOptions = {
